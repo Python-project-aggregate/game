6c9c7d5d73725089a20e5a1538e77e0ccbe2a0d8
hubhack <2329259856@qq.com>
2019-05-11 16:45:24 +0800

    add

---
+++ tic-tac-toe/ttt.py
@@ -0,0 +1,33 @@
+import os
+
+mat = {
+    '1':'.', '2':'.', '3':'.',
+    '4':'.', '5':'.', '6':'.',
+    '7':'.', '8':'.', '9':'.'
+}
+def print_board(board):
+    print(board['1'] + '|' + board['2'] + '|' + board['3'])
+    print('-+-+-')
+    print(board['4'] + '|' + board['5'] + '|' + board['6'])
+    print('-+-+-')
+    print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+def main():
+    begin = True
+    while begin:
+        begin = False
+        turn = 'x'
+        counter = 0
+
+        while counter < 9:
+            move = input('轮到{}走棋, 请输入位置:'.format(turn))
+            mat[move] = turn
+            print_board(mat)
+            if turn == 'x':
+                turn ='o'
+            else:
+                turn = 'x'
+            os.system('cls')
+
+if __name__ == '__main__':
+    main()
