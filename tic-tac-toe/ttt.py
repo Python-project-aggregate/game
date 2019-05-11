9f1c60d301616fc2dd63a8c5c8a3bc4300487b7a
hubhack <2329259856@qq.com>
2019-05-11 17:12:56 +0800

    add

--- tic-tac-toe/ttt.py
+++ tic-tac-toe/ttt.py
@@ -5,19 +5,27 @@ mat = {
     '4':'.', '5':'.', '6':'.',
     '7':'.', '8':'.', '9':'.'
 }
-# 判断是否满足条件
+#判断是否满足条件
 # def winer():
-#     for i in mat.values():
-#
-#
-#     if mat[1]==mat[2] ==mat[3]:
+#     if mat[1]==mat[2]==mat[3]:
 #         return True
+#     if mat[1]==mat[2]==mat[3]:
+#         return True
+#     if mat[1]==mat[2]==mat[3]:
+#         return True
+#     if mat[1]==mat[2]==mat[3]:
+
+
+
+
 def print_board(board):
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
     print('-+-+-')
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
     print(board['7'] + '|' + board['8'] + '|' + board['9'])
+
+def test_win():
 
 def main():
     begin = True
