f3e9cad1fc6435e5a6838729fd1beda90773f7e0
hubhack <2329259856@qq.com>
2019-06-07 10:11:42 +0800

    add

---
+++ game1/main.py
@@ -0,0 +1,31 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    # screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('打蚊子游戏')
+    # 设置背景色
+    # bg_color = (230, 230, 230)
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    # 开始游戏的主循环
+    while True:
+        # 监听键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.updata()
+        bullets.update()
+        # 删除子弹
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
+        print(len(bullets))
+        gf.update_screen(ai_settings, screen, ship, bullets)
+run_game()
